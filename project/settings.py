67d8578ede9bdcb82a97d6168cfb17c4eeee0a48
Dharshini <dharshinibaskaran15@gmail.com>
2026-06-29 17:36:11 +0530

    Updated settings for deployment

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c4d%$$j)xo3$uh$7bu3z-pf+vdy@y9edlm1o&^n-@fzt7g)j2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
